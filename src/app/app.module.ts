fbb9662d9d73091014de3d8da5fed3f6aa545ed8
animeshprm <ray.animesh@gmail.com>
2018-02-17 10:07:45 -0500

    creating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent 
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    AboutComponent 
   ],
   imports: [
     BrowserModule
